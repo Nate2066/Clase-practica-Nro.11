f823a8b857f1d2469c843500d940b11c0f4d2b21
OtherHmm <syrvm9@gmail.com>
2024-06-13 19:57:24 -0600

    Avance

---
+++ Gestion Hospital/Gestor.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Gestion_Hospital
+{
+    public class Gestor
+    {
+        public void VerEmpleados()
+        {
+
+        }
+
+        public void ExaminarEmpleado()
+        {
+
+        }
+
+        public void AgregarEmpleado()
+        {
+
+        }
+
+        public void EliminarEmpleado()
+        {
+
+        }
+
+
+
+
+    }
+}
--- Gestion Hospital/Program.cs
+++ Gestion Hospital/Program.cs
@@ -1,16 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Gestion_Hospital
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            //Variable base
+            bool Salir = false;
+            Gestor gestor = new Gestor();
 
+            //Bienvenida
+            Console.WriteLine("---------Binenvenido al sistema de gestion de empleados----------");
+            Console.WriteLine(".Click para continuar");
+            Console.ReadKey();
+            do
+            {
+                try
+                {
+                    //Menú :P
+                    Console.Clear();
+                    Console.WriteLine("==========GESTION DE EMPLEAOS==========");
+                    Console.WriteLine("*Que desea realizar?                    " + "\n");
+                    Console.WriteLine("1.   Ver lista de empleados");
+                    Console.WriteLine("2.   Agregar nuevo empleado");
+                    Console.WriteLine("3.   Eliminar empleados");
+                    Console.WriteLine("4.   Examimar empleados");
+                    Console.WriteLine("                             5.  *Salir*");
+                    Console.WriteLine("========================================");
+
+                    switch (Convert.ToByte(Console.ReadLine()))
+                    {
+                        case 1:
+                            gestor.VerEmpleados();
+                            break;
+                        case 2:
+                            gestor.AgregarEmpleado();
+                            break;
+                        case 3:
+                            gestor.EliminarEmpleado();
+                            break;
+                        case 4:
+                            gestor.ExaminarEmpleado();
+                            break;
+                        case 5:
+                            Salir = true;
+                            break;
+                        default:
+                            Console.WriteLine("Error! opcion invalida");
+                            break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Error! opcion invalida");
+                    Console.ReadKey();
+                }
+            } while (!Salir);
         }
     }
 }
