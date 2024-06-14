2b3fce26dd4cb86bc0dc5f722a5c90908c2166a8
OtherHmm <syrvm9@gmail.com>
2024-06-13 19:36:48 -0600

    Base

---
+++ Gestion Hospital/Emplado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Gestion_Hospital
+{
+    public class Empleado
+    {
+
+        string Nombre;
+        byte Experiencia;
+        double Salario;
+
+        public Empleado(string nombre, byte experiencia, double salario)
+        {
+            Nombre = nombre;
+            Experiencia = experiencia;
+            Salario = salario;
+        }
+
+        public virtual void CalcularSalario()
+        {
+
+            Salario = Salario * (Experiencia / 100);
+        }
+        public virtual void Examinar()
+        {
+            Console.WriteLine($"Nombre: {Nombre} \nAños de experiencia: {Experiencia}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Gestion Hospital/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Hospital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
