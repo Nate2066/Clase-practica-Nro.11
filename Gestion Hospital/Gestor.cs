72b6662bd818a93544018080a111bf2616ecd16a
OtherHmm <syrvm9@gmail.com>
2024-06-14 04:21:05 -0600

    Avance 3

--- Gestion Hospital/Gestor.cs
+++ Gestion Hospital/Gestor.cs
@@ -5,14 +5,38 @@ namespace Gestion_Hospital
 {
     public class Gestor //Manejo de operaciones
     {
+        // Base 
+        string[] empleados = new string[10];
+
+        //Metodos
+
         public void VerEmpleados()
         {
-            Console.WriteLine("Empleados:\n");
+            Console.WriteLine("---------Empleados----------\n");
+
+            for (int i = 0; i < empleados.Length; i++)
+            {
+                Console.WriteLine(empleados[i]);
+            }
         }
 
         public void ExaminarEmpleado()
         {
+            Console.Clear();
+            Console.WriteLine("---------Examinar empleados----------\n");
+            Console.WriteLine("Ingrese el nombre del empleado a buscar");
+            string nombre = Console.ReadLine();
+            for (int i = 0; i < empleados.Length; i++)
+            {
+                if (nombre == empleados[i])
+                {
+                    Empleado Empleado = new Empleado(nombre);
+                    Empleado.Examinar();
 
+                    break;
+                }
+            }
+            Console.ReadKey();
         }
 
         public void AgregarEmpleado()
@@ -22,18 +46,40 @@ namespace Gestion_Hospital
 
             Console.WriteLine("Ingrese el nombre del trabajador");
             string Nombre = Console.ReadLine();
+
+            Empleado empleado = new Empleado(Nombre);
+
             Console.WriteLine("Ingrese edad del trabajador");
             byte Edad = Convert.ToByte(Console.ReadLine());
             Console.WriteLine("Ingrese edad del trabajador");
             byte Experiencia = Convert.ToByte(Console.ReadLine());
 
-            Empleado empleado = new Empleado(Nombre,Edad,Experiencia);
+            empleado.Completar(Experiencia, Edad);
+
+            Console.WriteLine("Empleado añadido");
             Console.ReadKey();
         }
 
         public void EliminarEmpleado()
         {
+            Console.Clear();
+            Console.WriteLine("---------Eliminar empleados----------\n");
+            Console.WriteLine("Ingrese el nombre del empleado a buscar");
 
+            string nombre = Console.ReadLine();
+
+            foreach (string item in empleados)
+            {
+                if (nombre == item)
+                {
+                    Empleado Empleado = new Empleado(nombre);
+                    Empleado.Examinar();
+                    break;
+                }
+            }
+
+            Console.WriteLine("Empleado eliminado");
+            Console.ReadKey();
         }
     }
 }
